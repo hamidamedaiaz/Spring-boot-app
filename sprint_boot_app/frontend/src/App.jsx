acf9304f889b51d1b719dc73198d395c506597c6
hamidamediaz <amediazhamid736@gmail.com>
2026-01-18 15:43:24 +0100

    fronend initialisation   de react

---
+++ sprint_boot_app/frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '40px' }}>
+            <h1>✅ Frontend React opérationnel</h1>
+            <p>Prêt à se connecter au backend Spring Boot (API).</p>
+        </div>
+    )
+}
